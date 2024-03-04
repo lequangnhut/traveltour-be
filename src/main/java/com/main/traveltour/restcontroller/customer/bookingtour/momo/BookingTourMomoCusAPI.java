932a3fdf04ea3d4fbe836f3caa864725e2ff56e3
NHUTLQ <nhut.thanthien17@gmail.com>
2024-03-04 14:00:09 +0700

    thanh toán momo xong

---
+++ src/main/java/com/main/traveltour/restcontroller/customer/bookingtour/momo/BookingTourMomoCusAPI.java
@@ -0,0 +1,187 @@
+package com.main.traveltour.restcontroller.customer.bookingtour.momo;
+
+import com.main.traveltour.dto.customer.booking.BookingDto;
+import com.main.traveltour.dto.customer.booking.BookingToursDto;
+import com.main.traveltour.entity.*;
+import com.main.traveltour.service.UsersService;
+import com.main.traveltour.service.customer.BookingTourService;
+import com.main.traveltour.service.staff.TourDetailsService;
+import com.main.traveltour.service.utils.EmailService;
+import com.main.traveltour.utils.EntityDtoUtils;
+import com.main.traveltour.utils.GenerateNextID;
+import com.main.traveltour.utils.RandomUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("api/v1/")
+public class BookingTourMomoCusAPI {
+
+    @Autowired
+    private UsersService usersService;
+
+    @Autowired
+    private BookingTourService bookingTourService;
+
+    @Autowired
+    private TourDetailsService tourDetailsService;
+
+    @Autowired
+    private EmailService emailService;
+
+    /**
+     * Thêm mới tour với momo
+     */
+    @PostMapping("book-tour/create-book-tour-momo/{transactionId}")
+    private ResponseObject updateBookingTour(@RequestBody BookingDto bookingDto, @PathVariable int transactionId) {
+        BookingToursDto bookingToursDto = bookingDto.getBookingToursDto();
+        List<Map<String, String>> bookingTourCustomersDto = bookingDto.getBookingTourCustomersDto();
+
+        Integer userId = bookingToursDto.getUserId();
+        Integer totalAmountBook = bookingToursDto.getCapacityAdult() + bookingToursDto.getCapacityKid() + bookingToursDto.getCapacityBaby();
+        try {
+            if (transactionId != 0) {
+                if (userId != null) {
+                    BookingTours bookingTours = EntityDtoUtils.convertToEntity(bookingToursDto, BookingTours.class);
+                    bookingTours.setDateCreated(new Timestamp(System.currentTimeMillis()));
+                    createBookingTour(bookingToursDto, bookingTours, bookingTourCustomersDto, totalAmountBook, 1);
+
+                    createInvoices(bookingTours.getId());
+                    createContracts(bookingTours.getId());
+                    decreaseAmountTour(bookingTours.getTourDetailId(), totalAmountBook);
+                } else {
+                    createUser(bookingToursDto, bookingTourCustomersDto, totalAmountBook, transactionId);
+                }
+            } else {
+                if (userId != null) {
+                    BookingTours bookingTours = EntityDtoUtils.convertToEntity(bookingToursDto, BookingTours.class);
+                    bookingTours.setDateCreated(new Timestamp(System.currentTimeMillis()));
+                    createBookingTour(bookingToursDto, bookingTours, bookingTourCustomersDto, totalAmountBook, 2);
+
+                    createInvoices(bookingTours.getId());
+                    createContracts(bookingTours.getId());
+                    decreaseAmountTour(bookingTours.getTourDetailId(), totalAmountBook);
+                } else {
+                    createUser(bookingToursDto, bookingTourCustomersDto, totalAmountBook, transactionId);
+                }
+            }
+            return new ResponseObject("200", "Thành công", bookingDto);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseObject("404", "Thất bại", null);
+        }
+    }
+
+    private void createUser(BookingToursDto bookingToursDto, List<Map<String, String>> bookingTourCustomersDto, Integer totalAmountBook, int transactionId) {
+        String email = bookingToursDto.getCustomerEmail();
+        String phone = bookingToursDto.getCustomerPhone();
+        String citizenCard = bookingToursDto.getCustomerCitizenCard();
+
+        Users userPhone = usersService.findByPhone(phone);
+        Users userCitizenCard = usersService.findByCardId(citizenCard);
+        Optional<Users> currentUserOptional = Optional.ofNullable(usersService.findByEmail(email));
+
+        Users user = currentUserOptional.orElseGet(() -> {
+            Users newUser = new Users();
+            newUser.setEmail(bookingToursDto.getCustomerEmail());
+            newUser.setPassword(RandomUtils.RandomToken(10));
+            newUser.setFullName(bookingToursDto.getCustomerName());
+            if (userPhone == null) {
+                newUser.setPhone(bookingToursDto.getCustomerPhone());
+            } else if (userCitizenCard == null) {
+                newUser.setCitizenCard(bookingToursDto.getCustomerCitizenCard());
+            }
+            newUser.setIsActive(Boolean.TRUE);
+            usersService.authenticateRegister(newUser);
+            return newUser;
+        });
+
+        if (transactionId != 0) {
+            BookingTours bookingTours = EntityDtoUtils.convertToEntity(bookingToursDto, BookingTours.class);
+            bookingTours.setUserId(user.getId());
+            bookingTours.setDateCreated(new Timestamp(System.currentTimeMillis()));
+            createBookingTour(bookingToursDto, bookingTours, bookingTourCustomersDto, totalAmountBook, 1);
+
+            createInvoices(bookingTours.getId());
+            createContracts(bookingTours.getId());
+            decreaseAmountTour(bookingTours.getTourDetailId(), totalAmountBook);
+        } else {
+            BookingTours bookingTours = EntityDtoUtils.convertToEntity(bookingToursDto, BookingTours.class);
+            bookingTours.setUserId(user.getId());
+            bookingTours.setDateCreated(new Timestamp(System.currentTimeMillis()));
+            createBookingTour(bookingToursDto, bookingTours, bookingTourCustomersDto, totalAmountBook, 2);
+        }
+    }
+
+    private void createBookingTour(BookingToursDto bookingToursDto, BookingTours bookingTours, List<Map<String, String>> bookingTourCustomersDto, Integer totalAmountBook, int orderStatus) {
+        String bookingTourId = bookingToursDto.getId();
+
+        if (bookingToursDto.getPaymentMethod() == 3) { // 3: Momo
+            bookingTours.setOrderStatus(orderStatus);
+        }
+        bookingTourService.saveBookingTour(bookingTours);
+
+        createBookingTourCustomers(bookingTourId, bookingTourCustomersDto);
+    }
+
+    private void createBookingTourCustomers(String bookingTourId, List<Map<String, String>> bookingTourCustomersDto) {
+        for (Map<String, String> data : bookingTourCustomersDto) {
+            BookingTourCustomers bookingTourCustomers = new BookingTourCustomers();
+
+            for (Map.Entry<String, String> entry : data.entrySet()) {
+                String key = entry.getKey();
+                String value = entry.getValue();
+
+                if (key.startsWith("NameKH")) {
+                    int index = Integer.parseInt(key.substring(6));
+
+                    bookingTourCustomers.setBookingTourId(bookingTourId);
+                    bookingTourCustomers.setCustomerName(value);
+                    bookingTourCustomers.setCustomerBirth(Date.valueOf(data.get("BirthKH" + index)));
+                    bookingTourCustomers.setCustomerPhone(data.get("PhoneKH" + index));
+
+                    bookingTourService.saveBookingTourCustomer(bookingTourCustomers);
+                }
+            }
+        }
+    }
+
+    private void decreaseAmountTour(String tourDetailId, Integer totalAmountBook) {
+        TourDetails details = tourDetailsService.getById(tourDetailId);
+
+        int currentBookSeat = details.getBookedSeat();
+        int numberOfGuest = details.getNumberOfGuests();
+        int newBookSeat = currentBookSeat + totalAmountBook;
+
+        if (newBookSeat > numberOfGuest) {
+            details.setBookedSeat(currentBookSeat);
+            tourDetailsService.save(details);
+        } else {
+            details.setBookedSeat(currentBookSeat + totalAmountBook);
+            tourDetailsService.save(details);
+        }
+    }
+
+    private void createInvoices(String bookingTourId) {
+        Invoices invoices = new Invoices();
+        invoices.setId(GenerateNextID.generateNextInvoiceID(bookingTourService.findMaxCodeInvoices()));
+        invoices.setBookingTourId(bookingTourId);
+        invoices.setDateCreated(new Timestamp(System.currentTimeMillis()));
+        bookingTourService.saveBookingInvoices(invoices);
+    }
+
+    private void createContracts(String bookingTourId) {
+        Contracts contracts = new Contracts();
+        contracts.setId(GenerateNextID.generateNextContractID(bookingTourService.findMaxCodeContracts()));
+        contracts.setBookingTourId(bookingTourId);
+        contracts.setDateCreated(new Timestamp(System.currentTimeMillis()));
+        bookingTourService.saveBookingContracts(contracts);
+    }
+}
--- src/main/java/com/main/traveltour/restcontroller/customer/bookingtour/momo/MomoCusController.java
+++ src/main/java/com/main/traveltour/restcontroller/customer/bookingtour/momo/MomoCusController.java
@@ -6,14 +6,17 @@ import com.main.traveltour.configpayment.momo.enums.RequestType;
 import com.main.traveltour.configpayment.momo.models.PaymentResponse;
 import com.main.traveltour.configpayment.momo.processor.CreateOrderMoMo;
 import com.main.traveltour.configpayment.momo.shared.utils.LogUtils;
+import com.main.traveltour.utils.RandomUtils;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@RestController
+@Controller
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("api/v1")
 public class MomoCusController {
@@ -39,7 +42,30 @@ public class MomoCusController {
     }
 
     @GetMapping("momo/success-payment")
-    private String successOrderVNPay() {
-        return "Thanh toan thanh cong";
+    private String successOrderVNPay(HttpServletRequest request) {
+        String payType = request.getParameter("payType");
+        int paymentStatus = payType.equals("qr") ? 1 : 0;
+
+        String orderMessage = request.getParameter("orderInfo");
+        String tourDetailId = null;
+        String orderInfo = null;
+        int startIndex = orderMessage.lastIndexOf("-");
+        int startIndexOrderInfo = orderMessage.indexOf("#");
+        if (startIndex != -1 && startIndexOrderInfo != -1) {
+            orderInfo = orderMessage.substring(startIndexOrderInfo + 1);
+            tourDetailId = orderMessage.substring(startIndex - 6, startIndex);
+        }
+
+        String paymentTime = request.getParameter("responseTime");
+        String totalPrice = request.getParameter("amount");
+        int transactionId = RandomUtils.RandomOtpValue(5);
+
+        // Thêm thông tin vào URL redirect
+        return "redirect:http://localhost:3000/tour-detail/" + tourDetailId + "/booking-tour/customer-information/check-information/" +
+                (paymentStatus == 1 ? "payment-success" : "payment-failure") +
+                "?orderInfo=" + orderInfo +
+                (paymentStatus == 1 ? "&transactionId=" + transactionId : "&transactionId=0") +
+                "&paymentTime=" + paymentTime +
+                "&totalPrice=" + totalPrice;
     }
 }
