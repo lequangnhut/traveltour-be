fe8a37aa6b48c7cfc417cd1cce47626408733e1b
annt <anntpc04830@fpt.edu.vn>
2024-04-25 03:11:37 +0700

    - API thống kê xe

--- src/main/java/com/main/traveltour/repository/OrderTransportationsRepository.java
+++ src/main/java/com/main/traveltour/repository/OrderTransportationsRepository.java
@@ -42,4 +42,38 @@ public interface OrderTransportationsRepository extends JpaRepository<OrderTrans
 
     @Query("SELECT otr FROM OrderTransportations otr WHERE otr.orderStatus = :orderStatus AND otr.customerEmail = :email")
     Page<OrderTransportations> findAllBookingTransByUserId(@Param("orderStatus") Integer orderStatus, @Param("email") String email, Pageable pageable);
+
+    /**
+     * truy vấn tìm kiếm tổng số lượng đơn hàng của mỗi trạng thái của 1 năm qua từng trạng thái
+     * @param year năm cần tìm
+     * @param id mã của xe
+     * @return trả về số phần trăm của mỗi trạng thái
+     */
+    @Query(value = "SELECT YEAR(o.date_created)  AS year,\n" +
+            "       MONTH(o.date_created) AS month,\n" +
+            "       o.order_status,\n" +
+            "       COUNT(*),\n" +
+            "       ROUND((COUNT(*) / SUM(COUNT(*)) OVER (PARTITION BY YEAR(o.date_created), MONTH(o.date_created))) * 100, 1)\n" +
+            "FROM order_transportations o\n" +
+            "         INNER JOIN order_transportations_details od ON o.id = od.order_transportations_id\n" +
+            "         INNER JOIN transportation_schedules ts ON o.transportation_schedule_id = ts.id\n" +
+            "WHERE YEAR(o.date_created) = :year AND o.transportation_schedule_id = :id\n" +
+            "GROUP BY YEAR(o.date_created), MONTH(o.date_created), o.order_status", nativeQuery = true)
+    List<Object[]> findStatisticalBookingTransport(Integer year, String id);
+
+    /**
+     * truy vấn tổng số tiền qua từng tháng của 1 năm
+     * @param year năm
+     * @param id mã của hãng xe
+     * @return trả về tổng số tiền qua mỗi tháng
+     */
+    @Query(value = "SELECT YEAR(o.date_created)  AS year,\n" +
+            "       MONTH(o.date_created) AS month,\n" +
+            "       SUM(o.order_total) AS amount\n" +
+            "FROM order_transportations o\n" +
+            "         INNER JOIN order_transportations_details od ON o.id = od.order_transportations_id\n" +
+            "         INNER JOIN transportation_schedules ts ON o.transportation_schedule_id = ts.id\n" +
+            "WHERE YEAR(o.date_created) = :year AND o.transportation_schedule_id = :id AND o.order_status = 1\n" +
+            "GROUP BY YEAR(o.date_created), MONTH(o.date_created), o.order_status", nativeQuery = true)
+    List<Object[]> findTransportRevenueStatistics(Integer year, String id);
 }
